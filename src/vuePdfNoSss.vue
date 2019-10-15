735de613fd1289b2d7be87278dcf3afa1a333e93
veyhunk <veyhunk@qq.com>
2019-10-15 10:00:30 +0800

    fix h5 error

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,24 +1,23 @@
 <style src="./annotationLayer.css"></style>
 <script>
+import componentFactory from "./componentFactory.js";
+var component = componentFactory({});
+if (process) {
+  var __process__ = process;
+  if (__process__ && __process__.env && __process__.env.VUE_ENV !== "server") {
+    if (
+      typeof window !== "undefined" &&
+      "Worker" in window &&
+      navigator.appVersion.indexOf("MSIE 10") === -1
+    ) {
+      var PDFJS = require("pdfjs-dist/build/pdf.js");
+      var PdfjsWorker = require("worker-loader!pdfjs-dist/build/pdf.worker.js");
+      PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+    }
+    var pdfjsWrapper = require("./pdfjsWrapper.js").default;
+    component = componentFactory(pdfjsWrapper(PDFJS));
+  }
+}
 
-	import componentFactory from './componentFactory.js'
-
-	if ( process.env.VUE_ENV !== 'server' ) {
-
-		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-		var PDFJS = require('pdfjs-dist/build/pdf.js');
-
-		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/build/pdf.worker.js');
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
-		}
-
-		var component = componentFactory(pdfjsWrapper(PDFJS));
-	} else {
-
-		var component = componentFactory({});
-	}
-
-	export default component;
+export default component;
 </script>
